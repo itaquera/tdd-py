5e7178b92a43e33f4b2c3d3d4a16c1f760404e06
José Morais <jose.morais@tjro.jus.br>
2023-06-28 13:53:27 -0400

    Add teste para verificar número divisível por 3 e 5

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -12,5 +12,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_numero_divisivel_por_cinco(self):
         self.assertEqual(self.fb.buzz(5), "Buzz")
 
+    def test_numero_divisivel_por_tres_e_cinco(self):
+        self.assertEqual(self.fb.fizzbuzz(15), "FizzBuzz")
+
 if __name__ == '__main__':
     unittest.main()
