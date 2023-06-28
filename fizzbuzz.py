99de076facda806cf0d95a3bcb17ef16383400ff
José Morais <jose.morais@tjro.jus.br>
2023-06-28 13:59:46 -0400

    Add implementação do método para exibir todas as possibilidades

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,6 @@
 class FizzBuzz:
+    def __init__(self):
+        self.rules = [self.fizzbuzz, self.fizz, self.buzz]
 
     def fizz(self, number):
         if number % 3 == 0:
@@ -14,3 +16,7 @@ class FizzBuzz:
         if number % 15 == 0:
             return "FizzBuzz"
         return ""
+
+    def calculate(self, number):
+        result = "".join(rule(number) for rule in self.rules)
+        return result if result else str(number)
