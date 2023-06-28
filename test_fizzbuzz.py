bcbd1b3b450bd043c42a90a8b88547614f01ad6b
José Morais <jose.morais@tjro.jus.br>
2023-06-28 09:32:21 -0400

    Add teste para verificar número divisível por 3

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,5 +6,8 @@ class TestFizzBuzz(unittest.TestCase):
     def setUp(self):
         self.fb = FizzBuzz()
 
+    def test_numero_divisivel_por_tres(self):
+        self.assertEqual(self.fb.fizz(3), "Fizz")
+
 if __name__ == '__main__':
     unittest.main()
